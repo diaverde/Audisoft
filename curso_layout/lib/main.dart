8bc5bb3644af67c4f824a494d4afb0f9ef6acfe0
Andrés Sánchez <diaverde77@gmail.com>
2020-05-12 11:14:24 -0500

    Primera parte curso

--- curso_layout/lib/main.dart
+++ curso_layout/lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
   final String filmName = "Kung Fu Panda 3";
   final String userName = "LeiaSky77";
   final String userPic = 'images/user_pic.jpg';
-  final String userReview = 'Kung Fu Panda 3 es una buena película. Tiene buenos chistes, una trama bien construida, personajes cómicos, etc. '
-                            'El problema es que no es la primera, sino la tercera entrega de una saga. Todo eso ya estaba antes, y ahora no se '
-                            'aparta ni un ápice del camino ya recorrido…';
+  final String userReview = 'Kung Fu Panda 3 es una buena película. Tiene buenos chistes, una trama bien construida, personajes '
+                            'cómicos, etc. El problema es que no es la primera, sino la tercera entrega de una saga. Todo eso ya '
+                            'estaba antes, y ahora no se aparta ni un ápice del camino ya recorrido. Es hora de innovar o morir.';
   
   @override
   Widget build(BuildContext context) {
@@ -51,15 +51,15 @@ class MyApp extends StatelessWidget {
                 Row(
                   children: [                    
                     Text(
-                      'Película:',
+                      'Película: ',
                       style: TextStyle(
-                        color: Colors.grey[500],
+                        color: Colors.grey[800],
                       ),
                     ),
                     Text(
                       filmName,
                       style: TextStyle(
-                        color: Colors.grey[500],
+                        color: Colors.grey[600],
                       ),
                     ),
                   ],
@@ -68,15 +68,15 @@ class MyApp extends StatelessWidget {
                 Row(
                   children: [
                     Text(
-                      'Por:',
+                      'Por: ',
                       style: TextStyle(
-                        color: Colors.grey[500],
+                        color: Colors.grey[800],
                       ),
                     ),
                     Text(
                       userName,
                       style: TextStyle(
-                        color: Colors.grey[500],
+                        color: Colors.grey[600],
                       ),
                     ),
                   ],
@@ -97,16 +97,15 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    // Color a utilizar
-    Color color = Theme.of(context).primaryColor;
     // Sección de botones
     Widget buttonSection = Container(
       margin: const EdgeInsets.only(top: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(color, Icons.star_border, 'Me gusta'),
-          _buildButtonColumn(color, Icons.share, 'Compartir'),
+          //_buildButtonColumn(Icons.star_border, 'Me gusta'),
+          Favorite(),
+          _buildButtonColumn(Icons.share, 'Compartir'),
         ],
       ),
     );
@@ -135,24 +134,74 @@ class MyApp extends StatelessWidget {
   }
 
   // Función para construir cada botón
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
+  Column _buildButtonColumn(IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Icon(icon, color: color),
+        Icon(icon, color: Colors.blue),
         Container(
           margin: const EdgeInsets.only(top: 8),
           child: Text(
             label,
             style: TextStyle(
               fontSize: 14,
-              color: color,
-              decoration: TextDecoration.underline,  
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
             ),
           ),
         ),
       ],
     );
+  }
+}
+
+
+// Stateful Widget opcional para la estrella
+class Favorite extends StatefulWidget {
+  @override
+  _FavoriteState createState() => _FavoriteState();
+}
+
+// Estado correspondiente al Stateful Widget
+class _FavoriteState extends State<Favorite> {
+  // Variable para indicar si la reseña es "favorita"
+  bool _isFavorited = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    // Icono
+    Icon myIcon = _isFavorited ? Icon(Icons.star, color: Colors.red) : Icon(Icons.star_border, color: Colors.blue);
+    
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        InkWell(          
+          child: myIcon,
+          onTap: () => _toggleFavorite(),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            "Me gusta",
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.blue,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+      ],
+    );
+  }  
+
+  // Método para seleccionar o deseleccionar como favorita
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) _isFavorited = false;
+      else _isFavorited = true;      
+    });
   }
 }
